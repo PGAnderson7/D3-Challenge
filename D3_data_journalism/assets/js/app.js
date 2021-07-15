a9dc3420b552d08ac3696785b2a32bfbc2e54b72
Peter Anderson <peter.g.anderson1@gmail.com>
2021-07-14 23:47:14 -0500

    parse data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,181 @@
-// @TODO: YOUR CODE HERE!
+// SVG wrapper dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+  };
+  
+  var height = svgHeight - margin.top - margin.bottom;
+  var width = svgWidth - margin.left - margin.right;
+
+// append svg and group
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+var chosenYAxis = "obesity";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(censusData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.85,
+        d3.max(censusData, d => d[chosenXAxis]) * 1.15
+      ])
+      .range([0, width]);
+  
+    return xLinearScale;
+  
+  }
+
+// function used for updating y-scale var upon click on axis label
+function yScale(censusData, chosenYAxis) {
+    // create scales
+    var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(censusData, d => d[chosenYAxis]) * 0.85,
+        d3.max(censusData, d => d[chosenYAxis]) * 1.15
+      ])
+      .range([height, 0]);
+  
+    return yLinearScale;
+  
+  }
+
+// function used for updating xAxis var upon click on axis label
+function renderXAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+  }
+
+// function used for updating yAxis var upon click on axis label
+function renderYAxes(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(leftAxis);
+  
+    return yAxis;
+  }
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis]))
+      .attr("cy", d => newYScale(d[chosenYAxis]));
+  
+    return circlesGroup;
+  }
+
+function renderLabels(circleLabels, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+
+    circleLabels.transition()
+      .duration(1000)
+      .attr("x", d => newXScale(d[chosenXAxis]))
+      .attr("y", d => newYScale(d[chosenYAxis]) + 3);
+  
+    return circleLabels;
+  }
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
+
+    var xlabel;
+  
+    if (chosenXAxis === "poverty") {
+      xlabel = "Poverty";
+    }
+    else if (chosenXAxis === "age") {
+      xlabel = "Age:";
+    }
+    else {
+      xlabel = "Household Income";
+    }
+
+    var ylabel;
+  
+    if (chosenYAxis === "obesity") {
+      ylabel = "Obesity";
+    }
+    else if (chosenYAxis === "smokes") {
+      ylabel = "Smokes:";
+    }
+    else {
+      ylabel = "Lacks Healthcare:";
+    }
+
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -80])
+      .html(function(d) {
+        return (`${d.state}<br>${xlabel} ${d[chosenXAxis]}<br>${ylabel} ${d[chosenYAxis]}`);
+      });
+  
+    circlesGroup.call(toolTip);
+  
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+  
+    return circlesGroup;
+  }
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv").then(function(censusData, err) {
+    if (err) throw err;
+  
+    // parse data
+    censusData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.age = +data.age;
+        data.income = +data.income;
+        data.healthcare = +data.healthcare;
+        data.obesity = +data.obesity;
+        data.smokes = +data.smokes;
+    });
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(censusData, chosenXAxis);
+
+    // Create y scale function
+    var yLinearScale = yScale(censusData, chosenYAxis);
+    
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    var yAxis = chartGroup.append("g")
+        .classed("y-axis", true)
+        .call(leftAxis);
+
+    // append initial labels
+    
